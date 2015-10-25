18972006920c1c62d2dd933ab2e0558ded84e61b
Sean Bowe <ewillbefull@gmail.com>
2015-10-25 03:46:21 -0600

    Added notes for `IO` and `Transfers`.

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,8 @@ pub mod session_types;
 pub mod channels;
 mod protocol;
 
+pub use protocol::{Channel, Defer, Protocol, Handler, channel, channel_dual};
+
 #[macro_export]
 macro_rules! proto {
 	(@peano 0) => (Z);
@@ -89,8 +91,20 @@ macro_rules! handlers {
     );
 }
 
-pub use protocol::{Channel, Defer, Protocol, Handler, channel, channel_dual};
-
+/// This trait is implemented by backing IO structures to offer an
+/// interface for bi-directional channels. Discriminants are sent
+/// and received by `Channel` to indicate protocol changes; they
+/// tend to be smaller numbers, and so a variable length integer
+/// could be sent over a network instead of the raw `usize`.
+///
+/// As with all implementations of `Transfer<T>` for this concrete
+/// `IO`, if `IO` can guarantee that the backing channel is not
+/// accessed outside of these two traits, `Channel` can guarantee
+/// that these methods are only called when the data is expected
+/// over the channel. Over a network this expectation may not
+/// meet reality as there is no guarantee that the other side of
+/// the channel is implemented correctly. In that case,
+/// deserialization may be necessary.
 pub unsafe trait IO {
 	/// Closes the channel.
     unsafe fn close(&mut self);
@@ -102,10 +116,13 @@ pub unsafe trait IO {
     unsafe fn recv_varint(&mut self) -> Option<usize>;
 }
 
-/// This trait describes a backend for a channel to expose a message
-/// passing interface. The trait is unsafe because it will be asked to supply
-/// arbitrary types, and so must preserve the invariant that it will never allow
-/// the backing channel to be modified outside of this trait.
+/// An implementation of this trait provides sending and receiving
+/// functionality to `Channel` for an arbitrary `T`. `Channel` will
+/// only ever call these functions if it expects a `T`, so long as
+/// outside of this trait and `IO` the backing channel cannot be
+/// accessed.
+///
+/// See the explanation on `IO` for more details.
 pub unsafe trait Transfers<T>: IO {
     /// Sends an object from the handler to the outside channel.
     unsafe fn send(&mut self, T);
